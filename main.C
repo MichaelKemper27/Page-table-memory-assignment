fc8a9a3278813bfdc06b790a1deab2e62c985679
Rclodeca <ryan@theclodes.com>
2020-03-10 20:56:55 -0700

    update main

--- main.C
+++ main.C
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(int argc, char **argv){
-  PAGETABLE *p = new PAGETABLE(100);
-  p->RootNodePtr->callp();
+  PAGETABLE *p = new PAGETABLE(3);
+  //p->RootNodePtr->callp();
+  p->p();
+  cout << p->getOffset(0x12345678, 2) << endl;
+  p->PageInsert(0x12345678, 0x0);
+  cout << "last" << endl;
 }
